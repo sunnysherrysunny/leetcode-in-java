63b1c6e4ef9cdb35c7652fa0c3d199a94fa0d001
Sunny Guan <sunnyguan2018@gmail.com>
2022-12-08 19:26:05 -0500

    Q0938 recommited

--- Q0938.java
+++ Q0938.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Q0938 {
+public class Q0938 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.val >= low && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        } else {
+            return rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+    }
 }
