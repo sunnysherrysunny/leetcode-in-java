37debd6dbd4259e22d56df973effa82ed30a11e1
Sunny Guan <sunnyguan2018@gmail.com>
2022-12-08 19:21:26 -0500

    Q0876 finished

--- Q0876.java
+++ Q0876.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Q0876 {
+public class Q0876 {
+
+    public ListNode findMiddle(ListNode head, ListNode currMiddle, boolean move) {
+        if (head == null) {
+            return currMiddle;
+        }
+        if (move) {
+            return findMiddle(head.next, currMiddle.next, false);
+        } else {
+            return findMiddle(head.next, currMiddle, true);
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        return findMiddle(head, head, true);
+    }
 }
